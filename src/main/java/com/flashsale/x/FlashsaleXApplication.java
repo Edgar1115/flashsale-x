a314b635b8d476328706a665eec149c9075f7862
Edgar <edgar.zx.liao@outlook.com>
2026-06-05 21:50:03 +0800

    first commit

---
+++ src/main/java/com/flashsale/x/FlashsaleXApplication.java
@@ -0,0 +1,13 @@
+package com.flashsale.x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashsaleXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashsaleXApplication.class, args);
+	}
+
+}
