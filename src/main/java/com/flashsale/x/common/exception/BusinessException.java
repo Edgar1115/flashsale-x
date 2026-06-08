564d110ccf967b7df56b9482def2c1dd88eb0cd1
Edgar <edgar.zx.liao@outlook.com>
2026-06-08 10:59:43 +0800

    beifen

--- src/main/java/com/flashsale/x/common/exception/BusinessException.java
+++ src/main/java/com/flashsale/x/common/exception/BusinessException.java
@@ -3,10 +3,12 @@ package com.flashsale.x.common.exception;
 import com.flashsale.x.common.result.ResultCode;
 import lombok.Getter;
 
-@Getter
+@Getter // 自动生成getter方法
 public class BusinessException extends RuntimeException {
+    // RuntimeException：运行时异常，抛出时不需要try-catch
 
-    private final Integer code;
+    private final Integer code;         // 业务状态码
+
 
     public BusinessException(ResultCode resultCode) {
         super(resultCode.getMessage());
